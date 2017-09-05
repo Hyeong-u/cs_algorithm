f503e7e381e8789d8108e7dc4a004f2270333210
Kim Hyeongwoo <snake1754@gmail.com>
2017-09-05 12:14:09 +0900

    Solve problem 001

--- 001_Trucks/main.cpp
+++ 001_Trucks/main.cpp
@@ -1,21 +1,65 @@
 /*************************************************************************
-*																		 *
-* Problem:																 *
-* The 41st Annual ACM Asia Reginal Daejeon - Problem L Truck			 *
-*																		 *
+*
+* Problem:
+* ACM 2016 Asia Reginal Daejeon Nationwide Internet Competition
+*	Problem L Truck
 *************************************************************************/
 /*************************************************************************
-*																		 *
-* Kookmin Univ Computer Science junior          						 *
-* 20153168 Kim Hyeongwoo												 *
-*																		 *
+*
+* Kookmin Univ Computer Science junior
+* 20153168 Kim Hyeongwoo
+*
 *************************************************************************/
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
+
+int CalculateTime(int width, int max_load, queue<int> trucks)
+{
+    int load = 0;
+    int second = 0;
+    queue<int> bridge;
+    for (int i = 0; i < width; i++)
+    {
+        bridge.push(0);
+    }
+    while (!trucks.empty())
+    {
+        load -= bridge.front();
+        bridge.pop();
+        if (load + trucks.front() <= max_load)
+        {
+            load += trucks.front();
+            bridge.push(trucks.front());
+            trucks.pop();
+        }
+        else
+        {
+            bridge.push(0);
+        }
+        second++;
+    }
+    return second + width;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int num_testcases;
+    cin >> num_testcases;
+    int num_trucks, width, max_load, weight;
+
+    for (int i = 0; i < num_testcases; i++)
+    {
+        queue<int> trucks;
+        cin >> num_trucks >> width >> max_load;
+        for (int j = 0; j < num_trucks; j++)
+        {
+            cin >> weight;
+            trucks.push(weight);
+        }
+        cout << CalculateTime(width, max_load, trucks) << endl;
+    }
     return 0;
 }
