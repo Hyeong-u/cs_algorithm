51113d6c46740bb85fca0dbbe96d510312e10d7d
Kim Hyeongwoo <snake1754@gmail.com>
2017-09-11 22:31:59 +0900

    Solve problem 003

--- 003_QIndex/main.cpp
+++ 003_QIndex/main.cpp
@@ -11,11 +11,40 @@
 *
 *************************************************************************/
 #include <iostream>
+#include <vector>
+#include <algorithm>
+//#include <fstream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    ios_base::sync_with_stdio(false);
+//    ifstream cin;
+//    cin.open("input.txt");
+//    if (cin.fail()) {
+//        cout << "input.txt opening is failed.\n";
+//        exit(1);
+//    }
+    int num_testcases;
+    cin >> num_testcases;
+    for (int i = 0; i < num_testcases; i++) {
+        int num_papers;
+        cin >> num_papers;
+        vector<int> papers;
+        papers.reserve(num_papers + 1);
+        for (int j = 0; j < num_papers; j++) {
+            cin >> papers[j];
+        }
+        sort(papers.begin(), papers.begin() + num_papers);
+        int ans = 0;
+        for (int j = 0; j < num_papers; j ++) {
+            if (papers[j] >= num_papers - j) {
+                ans = num_papers - j;
+                break;
+            }
+        }
+        cout << ans << "\n";
+    }
     return 0;
 }
